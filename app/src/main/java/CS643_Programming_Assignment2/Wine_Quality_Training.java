74d895ad9c83bac9bec722a81b9bd51a26e29f71
pookri <sutharpooja2508@gmail.com>
2022-11-27 18:45:06 -0500

    LRModel added

---
+++ app/src/main/java/CS643_Programming_Assignment2/Wine_Quality_Training.java
@@ -0,0 +1,81 @@
+package CS643_Programming_Assignment2;
+
+import java.io.IOException;
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
+import org.apache.spark.ml.feature.StandardScaler;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.ml.classification.*;
+
+
+public class Wine_Quality_Training {
+
+    private static class CommonData { 
+        VectorAssembler assembler;
+        StandardScaler scaler;
+        Dataset<Row> trainingData;
+    }
+
+    public static Dataset<Row> cleanData(SparkSession spark) {
+
+        Dataset<Row> training = spark.read()
+        .option("header", true)
+        .option("multiline", true).option("quote", "\"")
+        .option("inferSchema", true)
+        .option("delimiter", ";")
+        .csv( "app\\src\\main\\resources\\Traindata.csv" );
+
+        training = training.na().drop().cache();
+        String[] headersTrain = training.schema().names();
+        for (String name : headersTrain){ 
+            training=training.withColumnRenamed(name, name.replace('"', ' ').trim());
+        }
+        
+        return training;
+
+    }
+
+    public static CommonData createPipeline(SparkSession spark){ 
+        CommonData commonData = new CommonData();
+        Dataset<Row> training=cleanData(spark);
+        String [] headersTrain = training.schema().names();
+        VectorAssembler assemblerTrain = new VectorAssembler()
+        .setInputCols(headersTrain).setOutputCol("features");
+
+        StandardScaler scaler = new StandardScaler().setInputCol("features").setOutputCol("Scaled_feature");
+        commonData.assembler = assemblerTrain;
+        commonData.scaler = scaler;
+        commonData.trainingData = training;
+        return commonData;
+    }
+
+    public static void logisticRegrssionModel(SparkSession spark) throws IOException{
+
+        CommonData data = createPipeline(spark);
+
+        LogisticRegression logisticRegression = new LogisticRegression();
+
+        logisticRegression.setLabelCol("quality").setFeaturesCol("features");
+        Pipeline pipeline = new Pipeline();
+        
+        pipeline.setStages( new PipelineStage[]{data.assembler,data.scaler,logisticRegression});
+        PipelineModel model = pipeline.fit(data.trainingData);
+        
+        // LogisticRegressionModel lrModel = (LogisticRegressionModel)(model.stages()[2]);
+        model.write().overwrite().save("app\\src\\main\\resources\\outputModel\\logisticRegression");
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        SparkSession spark = SparkSession.builder().appName("JavaLinerRegressionModel").master("local").getOrCreate();    
+        logisticRegrssionModel(spark);
+        spark.stop();
+    }
+
+
+    
+}
